a713d51e18032201540e8ffb8c7519b935d98018
Onaniyi Taofeek <39240106+TaofeekS@users.noreply.github.com>
2025-05-13 22:16:14 +0100

    day 3

--- cookinggames/Resource/Script/OrderData.gd
+++ cookinggames/Resource/Script/OrderData.gd
@@ -6,3 +6,12 @@ class_name OrderData
 @export var foodOrder : FoodData
 
 @export var hatedIngredientsData : Array[IngredientData]
+
+
+func satisfactionPoint(givenfood :FoodData, ingredientsUsedData : Array[IngredientData]):
+	if givenfood.foodName != foodOrder.foodName:
+		return 0
+	
+	var currentSatisfactionPoint = 100.0
+	
+	return currentSatisfactionPoint
